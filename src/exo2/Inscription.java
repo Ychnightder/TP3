a736a8bb1d986a994a4300471fa0d0984a9fd294
BaZz Ychnightder <pierreychnightder561@gmail.com>
2025-02-15 20:29:15 +0100

    TD3 fini

---
+++ src/exo2/Inscription.java
@@ -0,0 +1,23 @@
+package exo2;
+
+
+public class Inscription implements Runnable {
+
+	private Cours cours;
+	private int nbPlaces;
+
+	public Inscription(Cours cours, int nbPlaces) {
+		this.cours = cours;
+		this.nbPlaces = nbPlaces;
+	}
+	public void run() {
+		try {
+			synchronized (cours) {
+				cours.inscription(nbPlaces);
+			}
+		} catch (PasAsseezDePlacesException e) {
+			System.out.println(e);
+		}
+	}
+
+}
